7666239453eb396e37f428accdc83e32b2c5601c
amaninvestationteam <aman.dev@investationteam.co.in>
2026-02-10 16:43:25 +0530

    refactor(sidenavbar): remove UI component

--- src/configs/navigation.config/index.ts
+++ src/configs/navigation.config/index.ts
@@ -1,5 +1,4 @@
 import dashboardsNavigationConfig from './dashboards.navigation.config'
-import uiComponentNavigationConfig from './ui-components.navigation.config'
 import conceptsNavigationConfig from './concepts.navigation.config'
 import authNavigationConfig from './auth.navigation.config'
 import othersNavigationConfig from './others.navigation.config'
@@ -9,7 +8,7 @@ import type { NavigationTree } from '@/@types/navigation'
 const navigationConfig: NavigationTree[] = [
     ...dashboardsNavigationConfig,
     ...conceptsNavigationConfig,
-    ...uiComponentNavigationConfig,
+   // ...uiComponentNavigationConfig,
     ...authNavigationConfig,
     ...othersNavigationConfig,
     ...guideNavigationConfig,
